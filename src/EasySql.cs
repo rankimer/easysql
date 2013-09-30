b5f3da78e9f49ed3340d8e39d1469f3826cb6d05
kimerran <kimerran@users.noreply.github.com>
2013-09-30 17:57:29 +0800

    Update EasySql.cs

--- src/EasySql.cs
+++ src/EasySql.cs
@@ -1,24 +1,25 @@
+
 /*
-    The MIT License (MIT)
-    Copyright (c) 2013 kimerran
+The MIT License (MIT)
+Copyright (c) 2013 kimerran
 
-    Permission is hereby granted, free of charge, to any person obtaining a copy of
-    this software and associated documentation files (the "Software"), to deal in
-    the Software without restriction, including without limitation the rights to
-    use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of
-    the Software, and to permit persons to whom the Software is furnished to do so,
-    subject to the following conditions:
+Permission is hereby granted, free of charge, to any person obtaining a copy of
+this software and associated documentation files (the "Software"), to deal in
+the Software without restriction, including without limitation the rights to
+use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of
+the Software, and to permit persons to whom the Software is furnished to do so,
+subject to the following conditions:
 
-    The above copyright notice and this permission notice shall be included in all
-    copies or substantial portions of the Software.
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
 
-    THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
-    IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS
-    FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR
-    COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER
-    IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN
-    CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
- */
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS
+FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR
+COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER
+IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN
+CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+*/
 #region Select your DB type
 #define SQLSERVER
 //#define MSACCESS
@@ -38,17 +39,15 @@ using System.Data.Common;
 #region Include DB type Libraries
 #if MSACCESS
 using System.Data.OleDb;
-#endif
-#if SQLSERVER
+#elif SQLSERVER
 using System.Data.SqlClient;
-#endif
-#if MYSQL
+#elif MYSQL
 using System.Data.MySql;
-#endif
-#if SQLITE
+#elif SQLITE
 using System.Data.SQLite;
-#endif
-#if POSTGRESQL
+#elif ORACLE
+using Oracle
+#elif POSTGRESQL
 using Npgsql;
 #endif
 #endregion
@@ -58,182 +57,153 @@ namespace Kimerran.EasySql
     /// <summary>
     /// EasySql is a simple wrapper to common databases used for a .NET application
     /// </summary>
-    public class EasySql
+    public class EasySql 
     {
+       
+        #region private members
         /// <summary>
-        /// Instance of EasySqlDb which hides the implementation
+        /// Internal connection object
         /// </summary>
-        private EasySqlDb db;
+        private DbConnection _cn;
 
         /// <summary>
-        /// Creates instance of EasySql wrapper
+        /// Internal command object
         /// </summary>
-        /// <param name="connectionString"></param>
+        private DbCommand _cm;
+
+        /// <summary>
+        /// Calls Open() method of the connection object
+        /// </summary>
+        private void OpenDbConnection()
+        {
+            _cn.Open();
+        }
+
+        /// <summary>
+        /// Calls Close() method of the connection object
+        /// </summary>
+        private void CloseDbConnection()
+        {
+            _cn.Close();
+        }
+	    #endregion
+
+        #region constructor
+        /// <summary>
+        /// Creates instance of EasySql
+        /// </summary>
+        /// <param name="connectionString">Valid connection string for the selected database type</param>
         public EasySql(string connectionString)
         {
-            DbConnection cn = _CreateConnection();
-            DbCommand cm = _CreateCommand();
-            cn.ConnectionString = connectionString;
-            cm.Connection = cn;
+            _cn = _CreateConnection();
+            _cn.ConnectionString = connectionString;
 
-            db = new EasySqlDb(cn, cm);
-
+            _cm = _CreateCommand();
+            _cm.Connection =_cn;
+           
         }
+	    #endregion
+        
+        #region public members
 
         /// <summary>
-        /// Returns a boolean whether the connection is successful or not
+        /// Gets or sets the command timeout value of the command
         /// </summary>
-        /// <returns></returns>
-        public bool TryOpen()
-        {
-            try
+        public int  CommandTimeOut { 
+            get 
             {
-                db.OpenDbConnection();
-                return true;
+                if (_cm != null) 
+                {
+                    return _cm.CommandTimeout;
+                }
+                return 0;
             }
-            catch (Exception)
+            set
             {
-                return false;
-            }
+                if (_cm != null)
+                {
+                    _cm.CommandTimeout = value;
+                }
 
+            }
         }
-
-        /// <summary>
-        /// Used for SELECT queries and returns a DataTable containing the results
+       
+		/// <summary>
+        /// Implementation of ExecuteQuery used for SELECT queries
         /// </summary>
-        /// <param name="query"></param>
+        /// <param name="queryString"></param>
         /// <returns></returns>
-        public DataTable ExecuteQuery(string query)
+        public DataTable ExecuteQuery(string queryString)
         {
-            try
-            {
-                return db.ExecuteQuery(query);
-            }
-            catch (Exception e)
-            {
-                
-                throw new Exception(e.Message);
-            }
+            this.OpenDbConnection();
+            DataTable retDt = new DataTable();
+            DbDataReader dr;
+
+            _cm.CommandText = queryString;
+            _cm.CommandType = CommandType.Text;
+
+            dr = _cm.ExecuteReader();
          
+            retDt.Load(dr);
+
+            this.CloseDbConnection();
+
+            return retDt;
         }
 
         /// <summary>
-        /// Used for INSERT, UPDATE and DELETE and returns the number of affected rows
+        /// Implementation of ExecuteNonQuery used for INSERT, UPDATE and DELETE queries
         /// </summary>
-        /// <param name="query"></param>
+        /// <param name="queryString"></param>
         /// <returns></returns>
-        public int ExecuteNonQuery(string query)
+        public int ExecuteNonQuery(string queryString)
         {
-            try
-            {
-                return db.ExecuteNonQuery(query);
-            }
-            catch (Exception e)
-            {
-                
-                throw new Exception(e.Message);
-            }
+            this.OpenDbConnection();
+            int retRowsAffected;
+
+            _cm.CommandText = queryString;
+            _cm.CommandType = CommandType.Text;
+
+            retRowsAffected = _cm.ExecuteNonQuery();
+
+            this.CloseDbConnection();
+
+            return retRowsAffected;
+
         }
 
-
-        #region EasySqlDb Class
         /// <summary>
-        /// Hides the implementation of EasySql
+        /// 
         /// </summary>
-        protected class EasySqlDb
+        /// <param name="storedProcedureName"></param>
+        [System.Obsolete("Feature not yet implemented")]
+        public DataTable? ExecuteStoredProcedure(string storedProcedureName)
         {
-            /// <summary>
-            /// Internal connection object
-            /// </summary>
-            private DbConnection _cn;
-
-            /// <summary>
-            /// Internal command object
-            /// </summary>
-            private DbCommand _cm;
-
-            /// <summary>
-            /// Creates instance of EasySqlDb
-            /// </summary>
-            /// <param name="connection"></param>
-            /// <param name="command"></param>
-            public EasySqlDb(DbConnection connection, DbCommand command)
-            {
-                _cn = connection;
-                _cm = command;
-            }
-
-            /// <summary>
-            /// Calls Open() method of the connection object
-            /// </summary>
-            public void OpenDbConnection()
-            {
-                _cn.Open();
-            }
-
-            /// <summary>
-            /// Calls Close() method of the connection object
-            /// </summary>
-            public void CloseDbConnection()
-            {
-                _cn.Close();
-            }
-
-            /// <summary>
-            /// Implementation of ExecuteQuery used for SELECT queries
-            /// </summary>
-            /// <param name="queryString"></param>
-            /// <returns></returns>
-            public DataTable ExecuteQuery(string queryString)
-            {
-                this.OpenDbConnection();
-                DataTable retDt = new DataTable();
-                DbDataReader dr;
-
-                _cm.CommandText = queryString;
-                _cm.CommandType = CommandType.Text;
-
-                 dr = _cm.ExecuteReader();
-
-                 retDt.Load(dr);             
-
-                this.CloseDbConnection();
-
-                return retDt;
-            }
-
-            /// <summary>
-            /// Implementation of ExecuteNonQuery used for INSERT, UPDATE and DELETE queries
-            /// </summary>
-            /// <param name="queryString"></param>
-            /// <returns></returns>
-            public int ExecuteNonQuery(string queryString)
-            {
-                this.OpenDbConnection();
-                int retRowsAffected;
-
-                _cm.CommandText = queryString;
-                _cm.CommandType = CommandType.Text;
-
-                retRowsAffected = _cm.ExecuteNonQuery();
-
-                this.CloseDbConnection();
-
-                return retRowsAffected;
-                
-            }
-
+            //return null;
+            throw new Exception("Feature not yet implemented");
         }
-        #endregion
 
-        #region DB-type specifics
+        
         /// <summary>
-        /// Creates a DbCommand object depending on which database you choose
+        /// Dispose the EasySql instance
+        /// </summary>
+        public void Dispose()
+        {
+            _cn.Dispose();
+            _cm.Dispose();
+        }
+
+	    #endregion
+        
+
+        #region db-type specific
+        /// <summary>
+        /// Creates a command object for the selected database type
         /// </summary>
         /// <returns></returns>
         private DbCommand _CreateCommand()
         {
-#if SQLSERVER            
+#if SQLSERVER
             return new SqlCommand();
 #elif MSACCESS
             return new OleDbCommand();
@@ -241,13 +211,12 @@ namespace Kimerran.EasySql
         }
 
         /// <summary>
-        /// Creates a DbConnection object depending on which database you choose
+        /// Creates a connection object for the selected database type
         /// </summary>
         /// <returns></returns>
         private DbConnection _CreateConnection()
-        {
+        {   
 #if SQLSERVER
-            
             return new SqlConnection();
 #elif MSACCESS
             return new OleDbConnection();
@@ -255,8 +224,15 @@ namespace Kimerran.EasySql
         }
         #endregion
 
+        
+           
     }
 
 
 
+       
 }
+
+
+
+
